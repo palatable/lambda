7c1e74b72c5f766cacca52ad3c1f0028de3e8abd
jnape <jnape09@gmail.com>
2016-07-16 18:16:47 -0500

    Adding impure list and collection lenses; renaming UnsafeMapLens to ImpureMapLens

---
+++ src/main/java/com/jnape/palatable/lambda/lens/lenses/ImpureListLens.java
@@ -0,0 +1,22 @@
+package com.jnape.palatable.lambda.lens.lenses;
+
+import com.jnape.palatable.lambda.lens.Lens;
+
+import java.util.List;
+
+import static com.jnape.palatable.lambda.lens.Lens.simpleLens;
+
+public final class ImpureListLens {
+
+    private ImpureListLens() {
+    }
+
+    public static <X> Lens.Simple<List<X>, X> at(int index) {
+        return simpleLens(xs -> xs.size() > index ? xs.get(index) : null,
+                          (xs, x) -> {
+                              if (xs.size() > index)
+                                  xs.set(index, x);
+                              return xs;
+                          });
+    }
+}
