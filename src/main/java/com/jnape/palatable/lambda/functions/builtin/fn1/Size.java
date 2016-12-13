c9fd01394f609e46a632b84345a714ff0fa63387
jnape <jnape09@gmail.com>
2016-12-12 22:08:36 -0600

    Adding size

---
+++ src/main/java/com/jnape/palatable/lambda/functions/builtin/fn1/Size.java
@@ -0,0 +1,33 @@
+package com.jnape.palatable.lambda.functions.builtin.fn1;
+
+import com.jnape.palatable.lambda.functions.Fn1;
+
+import java.util.Collection;
+
+public final class Size implements Fn1<Iterable<?>, Long> {
+
+    private static final Size INSTANCE = new Size();
+
+    private Size() {
+    }
+
+    @Override
+    public Long apply(Iterable<?> iterable) {
+        if (iterable instanceof Collection)
+            return (long) ((Collection) iterable).size();
+
+        long size = 0L;
+        for (Object ignored : iterable) {
+            size++;
+        }
+        return size;
+    }
+
+    public static Size size() {
+        return INSTANCE;
+    }
+
+    public static Long size(Iterable<?> iterable) {
+        return size().apply(iterable);
+    }
+}
---
+++ src/test/java/com/jnape/palatable/lambda/functions/builtin/fn1/SizeTest.java
@@ -0,0 +1,33 @@
+package com.jnape.palatable.lambda.functions.builtin.fn1;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static com.jnape.palatable.lambda.functions.builtin.fn1.Size.size;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+public class SizeTest {
+
+    @Test
+    public void countsElementsInIterable() {
+        assertEquals((Long) 0L, size(emptyList()));
+        assertEquals((Long) 3L, size(asList(1, 2, 3)));
+    }
+
+    @Test
+    public void optimizesForCollections() {
+        Collection<Integer> collection = spy(new ArrayList<Integer>() {{
+            add(1);
+            add(2);
+            add(3);
+        }});
+        when(collection.iterator()).thenThrow(new IllegalStateException("should not be using the iterator"));
+        assertEquals((Long) 3L, size(collection));
+    }
+}
