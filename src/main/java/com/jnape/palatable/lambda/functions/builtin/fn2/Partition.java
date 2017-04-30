9e24d54bcd3e5506b62001b38a83d3bcfabfc154
jnape <jnape09@gmail.com>
2017-04-30 13:41:55 -0500

    Loosening Partition constraint on Either up to CoProduct2

--- src/main/java/com/jnape/palatable/lambda/functions/builtin/fn2/Partition.java
+++ src/main/java/com/jnape/palatable/lambda/functions/builtin/fn2/Partition.java
@@ -1,6 +1,6 @@
 package com.jnape.palatable.lambda.functions.builtin.fn2;
 
-import com.jnape.palatable.lambda.adt.Either;
+import com.jnape.palatable.lambda.adt.coproduct.CoProduct2;
 import com.jnape.palatable.lambda.adt.hlist.Tuple2;
 import com.jnape.palatable.lambda.functions.Fn1;
 import com.jnape.palatable.lambda.functions.Fn2;
@@ -9,22 +9,22 @@ import java.util.Optional;
 import java.util.function.Function;
 
 import static com.jnape.palatable.lambda.adt.hlist.HList.tuple;
-import static com.jnape.palatable.lambda.functions.builtin.fn1.Id.id;
 import static com.jnape.palatable.lambda.functions.builtin.fn2.Filter.filter;
 import static com.jnape.palatable.lambda.functions.builtin.fn2.Map.map;
 
 /**
- * Given an <code>Iterable&lt;A&gt;</code> <code>as</code> and a disjoint mapping function <code>a -&gt; Either&lt;L,
- * R&gt;</code>, return a {@link Tuple2} over the lazily unwrapped left <code>L</code> and right <code>R</code> values
- * in the first and second slots, respectively. Note that while the tuple must be constructed eagerly, the left and
- * right iterables contained therein are both lazy, so comprehension over infinite iterables is supported.
+ * Given an <code>Iterable&lt;A&gt;</code> <code>as</code> and a disjoint mapping function <code>a -&gt;
+ * CoProduct2&lt;A, B&gt;</code>, return a {@link Tuple2} over the lazily unwrapped left <code>A</code> and right
+ * <code>B</code> values in the first and second slots, respectively. Note that while the tuple must be constructed
+ * eagerly, the left and right iterables contained therein are both lazy, so comprehension over infinite iterables is
+ * supported.
  *
  * @param <A> A type contravariant to the input Iterable element type
- * @param <B> The output left Iterable element type, as well as the Either L type
- * @param <C> The output right Iterable element type, as well as the Either R type
- * @see Either
+ * @param <B> The output left Iterable element type, as well as the CoProduct2 A type
+ * @param <C> The output right Iterable element type, as well as the CoProduct2 B type
+ * @see CoProduct2
  */
-public final class Partition<A, B, C> implements Fn2<Function<? super A, ? extends Either<B, C>>, Iterable<A>, Tuple2<Iterable<B>, Iterable<C>>> {
+public final class Partition<A, B, C> implements Fn2<Function<? super A, ? extends CoProduct2<B, C>>, Iterable<A>, Tuple2<Iterable<B>, Iterable<C>>> {
 
     private static final Partition INSTANCE = new Partition();
 
@@ -32,18 +32,14 @@ public final class Partition<A, B, C> implements Fn2<Function<? super A, ? exten
     }
 
     @Override
-    public Tuple2<Iterable<B>, Iterable<C>> apply(Function<? super A, ? extends Either<B, C>> function,
+    public Tuple2<Iterable<B>, Iterable<C>> apply(Function<? super A, ? extends CoProduct2<B, C>> function,
                                                   Iterable<A> as) {
-        Iterable<Either<B, C>> eithers = map(function, as);
+        Iterable<CoProduct2<B, C>> coproducts = map(function, as);
 
-        Iterable<B> lefts = unwrapRight(map(Either::invert, eithers));
-        Iterable<C> rights = unwrapRight(map(id(), eithers));
+        Iterable<B> lefts = map(Optional::get, filter(Optional::isPresent, map(CoProduct2::projectA, coproducts)));
+        Iterable<C> rights = map(Optional::get, filter(Optional::isPresent, map(CoProduct2::projectB, coproducts)));
 
         return tuple(lefts, rights);
-    }
-
-    private <X> Iterable<X> unwrapRight(Iterable<Either<?, X>> eithers) {
-        return map(Optional::get, filter(Optional::isPresent, map(Either::toOptional, eithers)));
     }
 
     @SuppressWarnings("unchecked")
@@ -52,12 +48,12 @@ public final class Partition<A, B, C> implements Fn2<Function<? super A, ? exten
     }
 
     public static <A, B, C> Fn1<Iterable<A>, Tuple2<Iterable<B>, Iterable<C>>> partition(
-            Function<? super A, ? extends Either<B, C>> function) {
+            Function<? super A, ? extends CoProduct2<B, C>> function) {
         return Partition.<A, B, C>partition().apply(function);
     }
 
     public static <A, B, C> Tuple2<Iterable<B>, Iterable<C>> partition(
-            Function<? super A, ? extends Either<B, C>> function,
+            Function<? super A, ? extends CoProduct2<B, C>> function,
             Iterable<A> as) {
         return Partition.<A, B, C>partition(function).apply(as);
     }
--- src/test/java/com/jnape/palatable/lambda/functions/builtin/fn2/PartitionTest.java
+++ src/test/java/com/jnape/palatable/lambda/functions/builtin/fn2/PartitionTest.java
@@ -1,9 +1,10 @@
 package com.jnape.palatable.lambda.functions.builtin.fn2;
 
-import com.jnape.palatable.lambda.adt.Either;
+import com.jnape.palatable.lambda.adt.coproduct.CoProduct2;
 import com.jnape.palatable.lambda.adt.hlist.Tuple2;
 import com.jnape.palatable.lambda.functions.Fn1;
 import com.jnape.palatable.traitor.annotations.TestTraits;
+import com.jnape.palatable.traitor.framework.Subjects;
 import com.jnape.palatable.traitor.runners.Traits;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,13 +13,14 @@ import testsupport.traits.FiniteIteration;
 import testsupport.traits.ImmutableIteration;
 import testsupport.traits.Laziness;
 
-import static com.jnape.palatable.lambda.adt.Either.left;
-import static com.jnape.palatable.lambda.adt.Either.right;
+import static com.jnape.palatable.lambda.adt.choice.Choice2.a;
+import static com.jnape.palatable.lambda.adt.choice.Choice2.b;
 import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
 import static com.jnape.palatable.lambda.functions.builtin.fn1.Cycle.cycle;
 import static com.jnape.palatable.lambda.functions.builtin.fn1.Id.id;
 import static com.jnape.palatable.lambda.functions.builtin.fn2.Partition.partition;
 import static com.jnape.palatable.lambda.functions.builtin.fn2.Take.take;
+import static com.jnape.palatable.traitor.framework.Subjects.subjects;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertThat;
 import static testsupport.matchers.IterableMatcher.iterates;
@@ -27,14 +29,15 @@ import static testsupport.matchers.IterableMatcher.iterates;
 public class PartitionTest {
 
     @TestTraits({Laziness.class, EmptyIterableSupport.class, FiniteIteration.class, ImmutableIteration.class})
-    public Fn1<? extends Iterable, ?> createTraitsLeftTestSubject() {
-        return partition(constantly(left(1))).andThen(Tuple2::_1);
+    public Subjects<Fn1<? extends Iterable, ?>> createTraitsTestSubject() {
+        return subjects(partition(constantly(a(1))).andThen(Tuple2::_1),
+                        partition(constantly(b(1))).andThen(Tuple2::_2));
     }
 
     @Test
-    public void partitionsIterableIntoLeftsAndRights() {
+    public void partitionsIterableIntoAsAndBs() {
         Iterable<String> strings = asList("one", "two", "three", "four", "five");
-        Tuple2<Iterable<String>, Iterable<Integer>> partition = partition(s -> s.length() % 2 == 1 ? left(s) : right(s.length()), strings);
+        Tuple2<Iterable<String>, Iterable<Integer>> partition = partition(s -> s.length() % 2 == 1 ? a(s) : b(s.length()), strings);
 
         assertThat(partition._1(), iterates("one", "two", "three"));
         assertThat(partition._2(), iterates(4, 4));
@@ -42,8 +45,8 @@ public class PartitionTest {
 
     @Test
     public void infiniteListSupport() {
-        Iterable<Either<String, Integer>> eithers = cycle(left("left"), right(1));
-        Tuple2<Iterable<String>, Iterable<Integer>> partition = partition(id(), eithers);
+        Iterable<CoProduct2<String, Integer>> coproducts = cycle(a("left"), b(1));
+        Tuple2<Iterable<String>, Iterable<Integer>> partition = partition(id(), coproducts);
 
         assertThat(take(3, partition._1()), iterates("left", "left", "left"));
         assertThat(take(3, partition._2()), iterates(1, 1, 1));
