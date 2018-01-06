71dd33a85aed060183f3e6ab5e9d7e1180952cf8
jnape <jnape09@gmail.com>
2018-01-06 13:12:40 -0600

    destructuring predicate spike

--- src/main/java/com/jnape/palatable/lambda/Spike.java
+++ src/main/java/com/jnape/palatable/lambda/Spike.java
@@ -1,19 +1,31 @@
 package com.jnape.palatable.lambda;
 
 import com.jnape.palatable.lambda.adt.Maybe;
-import com.jnape.palatable.lambda.adt.hlist.HList;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
 import com.jnape.palatable.lambda.adt.hlist.Tuple3;
 import com.jnape.palatable.lambda.adt.hlist.Tuple4;
+import com.jnape.palatable.lambda.functions.specialized.Predicate;
+import com.jnape.palatable.lambda.structural.Case;
 import com.jnape.palatable.lambda.structural.Match;
 import com.jnape.palatable.lambda.structural.Struct;
 
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.adt.hlist.HList.tuple;
 import static com.jnape.palatable.lambda.functions.builtin.fn2.Eq.eq;
 import static com.jnape.palatable.lambda.structural.Case.of;
 import static com.jnape.palatable.lambda.structural.Cases.cases;
 import static com.jnape.palatable.lambda.structural.CatchAll.__;
 import static com.jnape.palatable.lambda.structural.Struct.struct;
 
-public class SPike {
+public class Spike {
+
+    public static <A> Case.DestructuringPredicate<Maybe<A>, A> $just(Predicate<A> predicate) {
+        return m -> m.filter(predicate);
+    }
+
+    public static <A> Case.DestructuringPredicate<Maybe<A>, A> $just(A a) {
+        return $just(eq(a));
+    }
 
     public static void main(String[] args) {
 
@@ -21,7 +33,7 @@ public class SPike {
 
             @Override
             public Tuple4<String, Integer, String, Integer> unapply() {
-                return HList.tuple(getBar(), getBaz(), getBar().toUpperCase(), getBaz() * 2);
+                return tuple(getBar(), getBaz(), getBar().toUpperCase(), getBaz() * 2);
             }
 
             public String getBar() {
@@ -39,6 +51,13 @@ public class SPike {
         Struct._3<String, String, Integer> struct = struct(foo::getBar, foo::getBar, foo::getBaz);
         Match.Partial<Tuple3<String, String, Integer>, String> match = cases(of(eq("foo"), __, __, (x, y, z) -> x));
         Maybe<String> blah = struct.match(match);
+
+        Case.DestructuringPredicate<Maybe<Integer>, Integer> aPredicate = $just(eq(1));
+        Case.DestructuringPredicate<Maybe<Integer>, Integer> bPredicate = $just(eq(2));
+        Case.Partial<Tuple2<Maybe<Integer>, Maybe<Integer>>, Integer> of = of(aPredicate, bPredicate, (x, y) -> x + y);
+        Maybe<Integer> result = cases(of)
+                .apply(tuple(just(1), just(2)));
+        System.out.println(result);
 
         Maybe<String> apply = match.apply(struct.unapply());
 
