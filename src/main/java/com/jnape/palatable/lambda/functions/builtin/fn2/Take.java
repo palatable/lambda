9d243788025da828c7c26788861ffd7d8ba8050f
jnape <jnape09@gmail.com>
2018-02-24 18:21:12 -0600

    Take deforests

--- src/main/java/com/jnape/palatable/lambda/functions/builtin/fn2/Take.java
+++ src/main/java/com/jnape/palatable/lambda/functions/builtin/fn2/Take.java
@@ -2,7 +2,7 @@ package com.jnape.palatable.lambda.functions.builtin.fn2;
 
 import com.jnape.palatable.lambda.functions.Fn1;
 import com.jnape.palatable.lambda.functions.Fn2;
-import com.jnape.palatable.lambda.iteration.TakingIterator;
+import com.jnape.palatable.lambda.iteration.TakingIterable;
 
 /**
  * Lazily limit the <code>Iterable</code> to <code>n</code> elements by returning an <code>Iterable</code> that stops
@@ -22,7 +22,7 @@ public final class Take<A> implements Fn2<Integer, Iterable<A>, Iterable<A>> {
 
     @Override
     public Iterable<A> apply(Integer n, Iterable<A> as) {
-        return () -> new TakingIterator<>(n, as.iterator());
+        return new TakingIterable<>(n, as);
     }
 
     @SuppressWarnings("unchecked")
