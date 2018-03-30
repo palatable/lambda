f0009b832c1bbfd3bd41df3978f9461fed89dd18
jnape <jnape09@gmail.com>
2018-03-30 17:12:46 -0500

    Adding HMapLens, lenses for HMaps

---
+++ src/test/java/com/jnape/palatable/lambda/lens/lenses/HMapLensTest.java
@@ -0,0 +1,30 @@
+package com.jnape.palatable.lambda.lens.lenses;
+
+import com.jnape.palatable.lambda.adt.hmap.TypeSafeKey;
+import org.junit.Test;
+
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+import static com.jnape.palatable.lambda.adt.hmap.HMap.emptyHMap;
+import static com.jnape.palatable.lambda.adt.hmap.HMap.hMap;
+import static com.jnape.palatable.lambda.adt.hmap.HMap.singletonHMap;
+import static com.jnape.palatable.lambda.adt.hmap.TypeSafeKey.typeSafeKey;
+import static java.util.Arrays.asList;
+import static testsupport.assertion.LensAssert.assertLensLawfulness;
+
+public class HMapLensTest {
+
+    @Test
+    public void valueAt() {
+        TypeSafeKey<String> key = typeSafeKey();
+        assertLensLawfulness(HMapLens.valueAt(key),
+                             asList(emptyHMap(),
+                                    singletonHMap(key, "foo"),
+                                    hMap(key, "foo",
+                                         typeSafeKey(), "bar"),
+                                    singletonHMap(typeSafeKey(), "bar")),
+                             asList(nothing(),
+                                    just("foo"),
+                                    just("bar")));
+    }
+}
