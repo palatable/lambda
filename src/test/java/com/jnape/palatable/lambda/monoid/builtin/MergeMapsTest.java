d30f10b2dc4ea48295492e5b0e91af3a02485b50
Alexander Bandukwala <7h3kk1d@gmail.com>
2018-12-07 19:18:18 -0600

    Added MapMerge
    - Given a Map supplier and a Semigroup over B get a Monoid<Map<A, B>>

---
+++ src/test/java/com/jnape/palatable/lambda/monoid/builtin/MergeMapsTest.java
@@ -0,0 +1,43 @@
+package com.jnape.palatable.lambda.monoid.builtin;
+
+import com.jnape.palatable.lambda.monoid.Monoid;
+import com.jnape.palatable.lambda.semigroup.Semigroup;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.jnape.palatable.lambda.adt.hlist.HList.tuple;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.ToMap.toMap;
+import static com.jnape.palatable.lambda.monoid.builtin.MergeMaps.mergeMaps;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.singletonMap;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MergeMapsTest {
+    private static final Semigroup<Integer> ADD = (x, y) -> x + y;
+    private Monoid<Map<String, Integer>> merge;
+
+    @Before
+    public void setUp() {
+        merge = mergeMaps(HashMap::new, ADD);
+    }
+
+    @Test
+    public void identity() {
+        assertTrue(merge.identity().isEmpty());
+    }
+
+    @Test
+    public void monoid() {
+        assertEquals(singletonMap("foo", 1), merge.apply(emptyMap(), singletonMap("foo", 1)));
+        assertEquals(singletonMap("foo", 1), merge.apply(singletonMap("foo", 1), emptyMap()));
+        assertEquals(singletonMap("foo", 2),
+                merge.apply(singletonMap("foo", 1), singletonMap("foo", 1)));
+        assertEquals(toMap(HashMap::new, asList(tuple("foo", 1), tuple("bar", 1))),
+                merge.apply(singletonMap("foo", 1), singletonMap("bar", 1)));
+    }
+}
