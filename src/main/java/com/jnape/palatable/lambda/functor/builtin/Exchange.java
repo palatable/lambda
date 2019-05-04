739f8d7831387d6b2efda80da8c56378b35dd8ab
jnape <jnape09@gmail.com>
2019-05-04 15:17:48 -0500

    Improving Exchange docs

--- src/main/java/com/jnape/palatable/lambda/functor/builtin/Exchange.java
+++ src/main/java/com/jnape/palatable/lambda/functor/builtin/Exchange.java
@@ -1,6 +1,7 @@
 package com.jnape.palatable.lambda.functor.builtin;
 
 import com.jnape.palatable.lambda.functor.Profunctor;
+import com.jnape.palatable.lambda.optics.Iso;
 
 import java.util.function.Function;
 
@@ -21,30 +22,52 @@ public final class Exchange<A, B, S, T> implements Profunctor<S, T, Exchange<A, 
         this.bt = bt;
     }
 
+    /**
+     * Extract the mapping <code>S -&gt; A</code>.
+     *
+     * @return a <code>{@link Function}&lt;S, A&gt;</code>
+     */
     public Function<? super S, ? extends A> sa() {
         return sa;
     }
 
+    /**
+     * Extract the mapping <code>B -&gt; T</code>.
+     *
+     * @return a <code>{@link Function}&lt;B, T&gt;</code>
+     */
     public Function<? super B, ? extends T> bt() {
         return bt;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <Z, C> Exchange<A, B, Z, C> diMap(Function<? super Z, ? extends S> lFn,
                                              Function<? super T, ? extends C> rFn) {
         return new Exchange<>(lFn.andThen(sa), bt.andThen(rFn));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <Z> Exchange<A, B, Z, T> diMapL(Function<? super Z, ? extends S> fn) {
         return (Exchange<A, B, Z, T>) Profunctor.super.<Z>diMapL(fn);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <C> Exchange<A, B, S, C> diMapR(Function<? super T, ? extends C> fn) {
         return (Exchange<A, B, S, C>) Profunctor.super.<C>diMapR(fn);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <Z> Exchange<A, B, Z, T> contraMap(Function<? super Z, ? extends S> fn) {
         return (Exchange<A, B, Z, T>) Profunctor.super.<Z>contraMap(fn);
