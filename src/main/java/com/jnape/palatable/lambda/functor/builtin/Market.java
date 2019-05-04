a79e760f3796934e683fd9acfcceca6081cd03fc
jnape <jnape09@gmail.com>
2019-05-04 18:17:12 -0500

    Adding missing javadoc

--- src/main/java/com/jnape/palatable/lambda/functor/builtin/Market.java
+++ src/main/java/com/jnape/palatable/lambda/functor/builtin/Market.java
@@ -6,6 +6,7 @@ import com.jnape.palatable.lambda.functions.Fn1;
 import com.jnape.palatable.lambda.functor.Applicative;
 import com.jnape.palatable.lambda.functor.Cocartesian;
 import com.jnape.palatable.lambda.monad.Monad;
+import com.jnape.palatable.lambda.optics.Prism;
 
 import java.util.function.Function;
 
@@ -15,6 +16,14 @@ import static com.jnape.palatable.lambda.adt.choice.Choice2.b;
 import static com.jnape.palatable.lambda.functions.Fn1.fn1;
 import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
 
+/**
+ * A profunctor used to extract the isomorphic functions a {@link Prism} is composed of.
+ *
+ * @param <A> the output that might fail to be produced
+ * @param <B> the input that guarantees its output
+ * @param <S> the input that might fail to map to its output
+ * @param <T> the guaranteed output
+ */
 public final class Market<A, B, S, T> implements
         Monad<T, Market<A, B, S, ?>>,
         Cocartesian<S, T, Market<A, B, ?, ?>> {
@@ -27,11 +36,35 @@ public final class Market<A, B, S, T> implements
         this.sta = fn1(sta);
     }
 
+    /**
+     * Extract the mapping <code>B -&gt; T</code>.
+     *
+     * @return a <code>{@link Function}&lt;B, T&gt;</code>
+     */
+    public Fn1<? super B, ? extends T> bt() {
+        return bt;
+    }
+
+    /**
+     * Extract the mapping <code>S -&gt; {@link Either}&lt;T, A&gt;</code>.
+     *
+     * @return a <code>{@link Function}&lt;S, {@link Either}&lt;T, A&gt;&gt;</code>
+     */
+    public Fn1<? super S, ? extends Either<T, A>> sta() {
+        return sta;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <U> Market<A, B, S, U> pure(U u) {
         return new Market<>(constantly(u), constantly(left(u)));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <U> Market<A, B, S, U> flatMap(Function<? super T, ? extends Monad<U, Market<A, B, S, ?>>> f) {
         return new Market<>(b -> f.apply(bt().apply(b)).<Market<A, B, S, U>>coerce().bt().apply(b),
@@ -40,6 +73,9 @@ public final class Market<A, B, S, T> implements
                                             .apply(s).invert()).invert());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <U> Market<A, B, S, U> zip(Applicative<Function<? super T, ? extends U>, Market<A, B, S, ?>> appFn) {
         Market<A, B, S, Function<? super T, ? extends U>> marketF = appFn.coerce();
@@ -47,19 +83,17 @@ public final class Market<A, B, S, T> implements
                             s -> sta().apply(s).invert().zip(marketF.sta().apply(s).invert()).invert());
     }
 
-    public Fn1<? super B, ? extends T> bt() {
-        return bt;
-    }
-
-    public Fn1<? super S, ? extends Either<T, A>> sta() {
-        return sta;
-    }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <U> Market<A, B, S, U> fmap(Function<? super T, ? extends U> fn) {
         return diMapR(fn);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <C> Market<A, B, Choice2<C, S>, Choice2<C, T>> cocartesian() {
         return new Market<>(bt.fmap(Choice2::b),
@@ -67,22 +101,34 @@ public final class Market<A, B, S, T> implements
                                                      tOrA -> tOrA.match(t -> left(b(t)), Either::right)));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <R, U> Market<A, B, R, U> diMap(Function<? super R, ? extends S> lFn,
                                            Function<? super T, ? extends U> rFn) {
         return new Market<>(bt.fmap(rFn), sta.diMapL(lFn).diMapR(c -> c.biMapL(rFn)));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <R> Market<A, B, R, T> diMapL(Function<? super R, ? extends S> fn) {
         return (Market<A, B, R, T>) Cocartesian.super.<R>diMapL(fn);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <U> Market<A, B, S, U> diMapR(Function<? super T, ? extends U> fn) {
         return (Market<A, B, S, U>) Cocartesian.super.<U>diMapR(fn);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <R> Market<A, B, R, T> contraMap(Function<? super R, ? extends S> fn) {
         return (Market<A, B, R, T>) Cocartesian.super.<R>contraMap(fn);
--- src/main/java/com/jnape/palatable/lambda/functor/builtin/Tagged.java
+++ src/main/java/com/jnape/palatable/lambda/functor/builtin/Tagged.java
@@ -9,6 +9,13 @@ import java.util.function.Function;
 
 import static com.jnape.palatable.lambda.adt.choice.Choice2.b;
 
+/**
+ * Like {@link Const}, but the phantom parameter is in the contravariant position, and the value is in covariant
+ * position.
+ *
+ * @param <S> the phantom type
+ * @param <B> the value type
+ */
 public final class Tagged<S, B> implements Monad<B, Tagged<S, ?>>, Cocartesian<S, B, Tagged<?, ?>> {
     private final B b;
 
@@ -16,61 +23,99 @@ public final class Tagged<S, B> implements Monad<B, Tagged<S, ?>>, Cocartesian<S
         this.b = b;
     }
 
+    /**
+     * Extract the contained value.
+     *
+     * @return the value
+     */
     public B unTagged() {
         return b;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <C> Tagged<S, C> flatMap(Function<? super B, ? extends Monad<C, Tagged<S, ?>>> f) {
         return f.apply(b).coerce();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <C> Tagged<S, C> pure(C c) {
         return new Tagged<>(c);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <C> Tagged<S, C> fmap(Function<? super B, ? extends C> fn) {
         return Monad.super.<C>fmap(fn).coerce();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <C> Tagged<S, C> zip(Applicative<Function<? super B, ? extends C>, Tagged<S, ?>> appFn) {
         return Monad.super.zip(appFn).coerce();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <C> Tagged<S, C> discardL(Applicative<C, Tagged<S, ?>> appB) {
         return Monad.super.discardL(appB).coerce();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <C> Tagged<S, B> discardR(Applicative<C, Tagged<S, ?>> appB) {
         return Monad.super.discardR(appB).coerce();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <C> Tagged<Choice2<C, S>, Choice2<C, B>> cocartesian() {
         return new Tagged<>(b(b));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <Z, C> Tagged<Z, C> diMap(Function<? super Z, ? extends S> lFn,
                                      Function<? super B, ? extends C> rFn) {
         return new Tagged<>(rFn.apply(b));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <Z> Tagged<Z, B> diMapL(Function<? super Z, ? extends S> fn) {
         return (Tagged<Z, B>) Cocartesian.super.<Z>diMapL(fn);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <C> Tagged<S, C> diMapR(Function<? super B, ? extends C> fn) {
         return (Tagged<S, C>) Cocartesian.super.<C>diMapR(fn);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <Z> Tagged<Z, B> contraMap(Function<? super Z, ? extends S> fn) {
         return (Tagged<Z, B>) Cocartesian.super.<Z>contraMap(fn);
