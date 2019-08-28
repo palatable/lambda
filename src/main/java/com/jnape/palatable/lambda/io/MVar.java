0d3662a5bfdb5cc5cc07070dde8b08661f3d56b6
Alexander Bandukwala <7h3kk1d@gmail.com>
2019-08-27 19:49:46 -0500

    fix mvar implementation

--- src/main/java/com/jnape/palatable/lambda/io/MVar.java
+++ src/main/java/com/jnape/palatable/lambda/io/MVar.java
@@ -1,9 +1,15 @@
 package com.jnape.palatable.lambda.io;
 
+import com.jnape.palatable.lambda.functions.builtin.fn1.Id;
+
 import java.util.concurrent.atomic.AtomicReference;
 
+import static com.jnape.palatable.lambda.adt.Maybe.maybe;
 import static com.jnape.palatable.lambda.functions.Fn0.fn0;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
+import static com.jnape.palatable.lambda.functions.builtin.fn4.IfThenElse.ifThenElse;
 import static com.jnape.palatable.lambda.io.IO.io;
+import static com.jnape.palatable.lambda.io.IO.monitorSync;
 
 public final class MVar<A> {
 
@@ -14,14 +20,20 @@ public final class MVar<A> {
     }
 
     public IO<A> take() {
-        return io(ref::get);
+        return monitorSync(this, io(() -> maybe(ref.get()).peek(__ -> io(() -> ref.set(null)))))
+                .flatMap(ma -> ma
+                        .fmap(IO::io)
+                        .orElse(take()));
     }
 
     public IO<MVar<A>> put(A a) {
-        return io(() -> {
-            ref.set(a);
-            return this;
-        });
+        return monitorSync(this, io(() -> maybe(ref.get())
+                .fmap(constantly(false))
+                .orElseGet(() -> {
+                    ref.set(a);
+                    return true;
+                })))
+                .flatMap(ifThenElse(Id.id(), constantly(IO.io(this)), __ -> put(a)));
     }
 
     public static <A> IO<MVar<A>> newMVar() {
--- src/test/java/com/jnape/palatable/lambda/io/MVarTest.java
+++ src/test/java/com/jnape/palatable/lambda/io/MVarTest.java
@@ -1,21 +1,38 @@
 package com.jnape.palatable.lambda.io;
 
-import com.jnape.palatable.traitor.runners.Traits;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.monad.Monad;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Into.into;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Tupler2.tupler;
+import static com.jnape.palatable.lambda.io.IO.io;
+import static com.jnape.palatable.lambda.io.IO.memoize;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static testsupport.matchers.IOMatcher.yieldsValue;
 
-@RunWith(Traits.class)
 public class MVarTest {
 
-    @Test(timeout = 100)
+    @Test(timeout = 1000)
     public void putTake() {
         assertThat(MVar.<String>newMVar()
-                           .flatMap(mvar -> mvar.put("foo"))
-                           .flatMap(MVar::take),
-                   yieldsValue(equalTo("foo")));
+                        .flatMap(mvar -> mvar.put("foo"))
+                        .flatMap(MVar::take),
+                yieldsValue(equalTo("foo")));
+    }
+
+
+    @Test(timeout = 1000)
+    public void putPutTakeTake() {
+        IO<MVar<Integer>> mVarIO = memoize(MVar.newMVar());
+
+        IO<Tuple2<Integer, Integer>> join = Monad.join(mVarIO.flatMap(mvar1 -> mvar1.take().fmap(tupler(mvar1)))
+                .fmap(into((mvar, i) -> mvar.take().fmap(tupler(i)))));
+
+        mVarIO.flatMap(mvar -> mvar.put(1)).flatMap(mvar -> mvar.put(2)).discardL(join)
+                .flatMap(t2 -> io(() -> System.out.println(t2)))
+                .unsafePerformAsyncIO()
+                .join();
     }
 }
