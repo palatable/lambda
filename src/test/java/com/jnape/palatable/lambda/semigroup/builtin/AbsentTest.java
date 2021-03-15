5b1d7c61c3a180aabea02fc312c98b5e84012946
J. Armando Cordova - @corlaez <18663098+corlaez@users.noreply.github.com>
2021-03-15 11:57:13 -0400

    Improve AbsentTest coverage

--- src/test/java/com/jnape/palatable/lambda/semigroup/builtin/AbsentTest.java
+++ src/test/java/com/jnape/palatable/lambda/semigroup/builtin/AbsentTest.java
@@ -19,13 +19,12 @@ public class AbsentTest {
 
     @Test
     public void semigroup() {
-        Absent<Integer>    absent   = absent();
         Semigroup<Integer> addition = Integer::sum;
 
-        assertEquals(just(3), absent.apply(addition, just(1), just(2)));
-        assertEquals(nothing(), absent.apply(addition, nothing(), just(1)));
-        assertEquals(nothing(), absent.apply(addition, just(1), nothing()));
-        assertEquals(nothing(), absent.apply(addition, nothing(), nothing()));
+        assertEquals(just(3), absent(addition, just(1), just(2)));
+        assertEquals(nothing(), absent(addition, nothing(), just(1)));
+        assertEquals(nothing(), absent(addition, just(1), nothing()));
+        assertEquals(nothing(), absent(addition, nothing(), nothing()));
     }
 
     @Test
