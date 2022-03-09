328169c6b08575413ebfaad48f057b13a1108b2e
Alexander Bandukwala <7h3kk1d@gmail.com>
2022-03-08 18:18:39 -0600

    Remove unused imports

--- src/main/java/com/jnape/palatable/lambda/internal/iteration/PredicatedDroppingIterable.java
+++ src/main/java/com/jnape/palatable/lambda/internal/iteration/PredicatedDroppingIterable.java
@@ -1,20 +1,9 @@
 package com.jnape.palatable.lambda.internal.iteration;
 
 import com.jnape.palatable.lambda.functions.Fn1;
-import com.jnape.palatable.lambda.functions.builtin.fn2.DropWhile;
 import com.jnape.palatable.lambda.internal.ImmutableQueue;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
-
-import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
-import static com.jnape.palatable.lambda.functions.builtin.fn1.Repeat.repeat;
-import static com.jnape.palatable.lambda.functions.builtin.fn2.Any.any;
-import static com.jnape.palatable.lambda.functions.builtin.fn2.DropWhile.dropWhile;
-import static com.jnape.palatable.lambda.functions.builtin.fn3.Times.times;
-import static java.util.Collections.singletonList;
 
 public final class PredicatedDroppingIterable<A> implements Iterable<A> {
     private final ImmutableQueue<Fn1<? super A, ? extends Boolean>> predicates;
